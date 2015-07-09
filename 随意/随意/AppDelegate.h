e72c7726100e6f718b224c71dba1921a41685a37
lanou3g <2476537279@qq.com>
2015-07-09 09:42:48 +0800

    随意使用

---
+++ 随意/随意/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  随意
+//
+//  Created by lanou3g on 15/7/9.
+//  Copyright (c) 2015年 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
